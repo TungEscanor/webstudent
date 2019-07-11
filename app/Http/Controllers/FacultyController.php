f1963befd9329239453a40a5ed6a7f3d8925dddc
Trịnh Tùng <trinhtung95@gmail.com>
2019-07-11 22:32:06 +0700

    'add'

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\facultyRequest;
 use App\Models\Classes;
-use App\Models\faculty;
+use App\Models\Faculty;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
